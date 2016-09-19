1dc4dddcd14c212e14b06ff860c4fcccdac16f41
crazeejeeves <crazeejeeves@hotmail.com>
2016-09-19 14:13:29 +0800

    Added working directory override. Temporary workaround for some products

--- __main__.py
+++ __main__.py
@@ -1,10 +1,16 @@
 import unittest
+import os
+import sys
 
 import framework
 from framework.config import CommandLineConfiguration as Configuration
 from framework.filters import FilterSystem
 from framework.controller import TestController
 
+# TODO: Remove this or replace with a check to enforce a consistent working folder
+# Workaround to force the working folder to default to the location of the script
+# file. Specific for the current
+os.chdir(sys.path[0])
 
 framework.init_logging()
 
